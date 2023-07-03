ebdd03c47e4015492f19b1fe0e1b1bb52d47751b
Hailemelekot <110323716+Hailemelekotmelakie@users.noreply.github.com>
2023-07-03 16:46:40 +0300

    Delete _static directory

--- _static/_next/static/fjjiNu9sD8dk8Tjsp5W5h/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/74c33104554b02a1.css","static/chunks/pages/index-b893267848a68138.js"],"/404":["static/chunks/pages/404-84f814bab3633879.js"],"/_error":["static/chunks/pages/_error-e6359318fe16f140.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
